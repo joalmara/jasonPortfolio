c672a2d8fb687f1e402e3b790a2583a1464f7086
Jason Almaraz <joalmara@uci.edu>
2019-03-28 13:24:17 -0700

    Header

--- jasonPortfolio/src/app/app.module.ts
+++ jasonPortfolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
