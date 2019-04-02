13570eebee565d374691b936bc87459408e766ee
Jason Almaraz <joalmara@uci.edu>
2019-04-01 18:38:43 -0700

    Carousel

--- jasonPortfolio/src/app/app.module.ts
+++ jasonPortfolio/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { CanvasComponent } from './components/pages/canvas/canvas.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SliderComponent } from './slider/slider.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
@@ -15,10 +18,13 @@ import { CanvasComponent } from './components/pages/canvas/canvas.component';
     AboutComponent,
     HeaderComponent,
     CanvasComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    CarouselModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- jasonPortfolio/src/app/components/pages/canvas/canvas.component.ts
+++ jasonPortfolio/src/app/components/pages/canvas/canvas.component.ts
@@ -11,5 +11,17 @@ export class CanvasComponent implements OnInit {
 
   ngOnInit() {
   }
+  items = [
+    { title: 'Slide 1' },
+    { title: 'Slide 2' },
+    { title: 'Slide 3' },
+  ]
+  
+  addSlide() {
+    this.items.push({
+      title: `Slide 4`
+    });
+  }
+  
 
 }
